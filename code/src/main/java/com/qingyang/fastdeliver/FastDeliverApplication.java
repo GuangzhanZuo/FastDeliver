b7165c62dbb75c23dd1794491e83b56035544636
Qingyang <zguangzhan@163.com>
2023-12-13 14:36:34 +0800

    构建项目

---
+++ code/src/main/java/com/qingyang/fastdeliver/FastDeliverApplication.java
@@ -0,0 +1,13 @@
+package com.qingyang.fastdeliver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FastDeliverApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FastDeliverApplication.class, args);
+    }
+
+}
---
+++ code/src/test/java/com/qingyang/fastdeliver/FastDeliverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qingyang.fastdeliver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FastDeliverApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
